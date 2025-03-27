fe5d3547bc3978e4bfddb857111d128e3871e444
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2025-03-27 16:59:42 +0700

    init

---
+++ src/test/java/com/qbit/microservice/InteractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qbit.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InteractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
