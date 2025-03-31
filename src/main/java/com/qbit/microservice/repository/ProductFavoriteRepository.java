c6668d2b68151f55f89bbb56ff2ff9ad71bcebdb
Nguyễn Ngọc Truyện <nntruyen027@gmail.com>
2025-03-31 20:38:05 +0700

    Xong post, binh luan, thich

--- src/main/java/com/qbit/microservice/repository/ProductFavoriteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qbit.microservice.repository;
-
-import com.qbit.microservice.entity.ProductFavorite;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductFavoriteRepository extends JpaRepository<ProductFavorite, Long> {
-}
